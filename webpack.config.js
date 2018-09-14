1bcf936e5f71b17d633f6fd287cdaff8b65731e1
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-09-14 17:36:11 +0530

    Added env file for environment varibles

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,63 @@
 const path = require('path')
+const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const dotenv = require('dotenv')
 
-module.exports = (env, options) => ({
-  entry: [
-    './src/index.js'
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.scss$/,
-        use: [options.mode !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.css$/,
-        use: [options.mode !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader']
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      filename: 'index.html'
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].[contenthash].css'
-    })
-  ],
-  devtool: options.mode === 'production' ? '(none)' : 'source-map',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/',
-    filename: '[name].[contenthash].js'
-  },
-  devServer: {
-    contentBase: './dist',
-    historyApiFallback: true
-  }
-})
+module.exports = (env, options) => {
+  const envs = dotenv.config().parsed
+
+  const envKeys = Object.keys(envs).reduce((prev, next) => {
+    const data = {}
+    data[`process.env.${next}`] = JSON.stringify(envs[next])
+    return data
+  }, {})
+
+  return ({
+    entry: [
+      './src/index.js'
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        },
+        {
+          test: /\.scss$/,
+          use: [options.mode !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.css$/,
+          use: [options.mode !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader']
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    },
+    plugins: [
+      new CleanWebpackPlugin(['dist']),
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: 'index.html'
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash].css'
+      }),
+      new webpack.DefinePlugin(envKeys)
+    ],
+    devtool: options.mode === 'production' ? '(none)' : 'source-map',
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      publicPath: '/',
+      filename: '[name].[contenthash].js'
+    },
+    devServer: {
+      contentBase: './dist',
+      historyApiFallback: true
+    }
+  })
+}
